5617ee3c8d79d62bd2f75bb7ccc0ac2018ae438f
David Hernandez Azagury <davidhazagury@gmail.com>
2023-04-02 19:28:43 +0200

    Fix get endpoint automatically

--- app/services/agile_crm/get_endpoint.rb
+++ app/services/agile_crm/get_endpoint.rb
@@ -1,5 +1,6 @@
 module AgileCrm
   module GetEndpoint
+<<<<<<< Updated upstream
     def endpoint
       "#{@crm_api_endpoint}/#{get_current_path}"
     end
@@ -8,6 +9,23 @@ module AgileCrm
      
     def get_current_path
       self.class.module_parent.to_s.split('::')[1].downcase
+=======
+    MAPPED_ENDPOINTS = {
+      'Contacts': 'contacts',
+      'Opportunities': 'opportunity'
+    }
+
+    def endpoint
+      "#{@crm_api_endpoint}/#{get_current_path}"
+    end
+
+    def get_current_path
+      MAPPED_ENDPOINTS[get_module_name]
+    end
+
+    def get_module_name
+      self.class.module_parent.to_s.split('::')[1].to_sym
+>>>>>>> Stashed changes
     end
   end
 end
