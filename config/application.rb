ea4f159a173a570d4f9e9c8238709f1b9b5fa083
David Hernandez Azagury <davidhazagury@gmail.com>
2023-04-09 09:32:50 +0200

    Add db config

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module CrmMappedEndpointsService
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
     config.api_only = true
   end
 end
