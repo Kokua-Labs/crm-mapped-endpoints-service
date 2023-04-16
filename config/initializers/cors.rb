b6672471085856b33f9349ba470c6e193803922c
David Hernandez Azagury <davidhazagury@gmail.com>
2023-04-16 20:15:50 +0200

    Tet

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['HOST_ALLOWED'], 'localhost:3000', '127.0.0.1:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
